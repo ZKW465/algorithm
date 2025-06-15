895cb9158171185f27bea7a74fef5679b0483f35
ZKW465 <2695415649@qq.com>
2025-06-16 00:01:47 +0800

    I/O优化规范代码

--- 杂/IO优化.cpp
+++ 杂/IO优化.cpp
@@ -1,155 +1,135 @@
-constexpr int MAXSIZE = 1 << 20;
-struct Input {
-    char buf[MAXSIZE], *p1 = buf, *p2 = buf;
-    #define isdigit(x) ('0' <= x && x <= '9')
-    
-    #define gc()                                                                 \
-       (p1 == p2 &&(p2 =(p1 = buf) + fread(buf, 1, MAXSIZE, stdin), p1 == p2)    \
-            ? EOF                                                                \
-            : *p1++)
+constexpr int kBufSize = 1 << 20;
 
-    bool blank(char ch) {
-        return ch == ' ' || ch == '\n' || ch == '\r' || ch == '\t' || ch == EOF;
+struct Input {
+  char buf[kBufSize];
+  char *p1 = buf;
+  char *p2 = buf;
+
+  __attribute__((always_inline)) char gc() {
+    if (__builtin_expect(p1 == p2, 0)) {
+      p2 = (p1 = buf) + fread(buf, 1, kBufSize, stdin);
+      if (__builtin_expect(p1 == p2, 0)) return EOF;
     }
-    void tie(void* x) {}
-    template <typename T>
-    Input &operator>>(T &x) {
-        x = 0;
-        bool neg = false;
-        char ch = gc();
-        for (; !isdigit(ch); ch = gc()) {
-            if(ch == '-') {
-                neg = true;
-            }
-        }
-        for (; isdigit(ch); ch = gc()) {
-            x = (x << 3) + (x << 1) + ch - '0';
-        }
-        if (neg) {
-            x = -x;
-        }
-        return *this;
-    }
-    Input &operator>>(char &x) {
-        x = ' ';
-        for (; blank(x); x = gc());
-        return *this;
-    }
-    Input &operator>>(double &x) {
-        x = 0;
-        double tmp = 1;
-        bool neg = 0;
-        char ch = gc();
-        for (; !isdigit(ch); ch = gc()) {
-            if (ch == '-') {
-                neg = 1;
-            }
-        }
-        for (; isdigit(ch); ch = gc()) {
-            x = x * 10 + ch - '0';
-        }
-        if (ch == '.') {
-            for (ch = gc(); isdigit(ch); ch = gc()) {
-                tmp /= 10.0, x += tmp *(ch - '0');
-            }
-        }
-        if(neg) x = -x;
-        return *this;
-    }
-    Input &operator>>(string &s) {
-        s.clear();
-        char ch = gc();
-        for (; blank(ch); ch = gc());
-        for (; !blank(ch); ch = gc()) {
-            s += ch;
-        }
-        return *this;
-    }
-    # undef isdigit
-    # undef gc
+    return *p1++;
+  }
+  
+  __attribute__((always_inline)) bool isdigit(char c) { 
+    return c >= '0' && c <= '9'; 
+  }
+  
+  __attribute__((always_inline)) bool blank(char c) {
+    return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF;
+  }
+
+  void tie(void*) {}
+
+  template<typename T>
+  Input& operator>>(T& x) {
+    x = 0;
+    bool neg = false;
+    char ch = gc();
+    for (; !isdigit(ch); ch = gc())
+      if (ch == '-')
+        neg = true;
+    for (; isdigit(ch); ch = gc())
+      x = (x << 3) + (x << 1) + (ch ^ 48);
+    if (neg)
+      x = -x;
+    return *this;
+  }
+  Input& operator>>(char& x) {
+    x = gc();
+    for (; blank(x); x = gc());
+    return *this;
+  }
+  Input& operator>>(double& x) {
+    x = 0;
+    double tmp = 1;
+    bool neg = 0;
+    char ch = gc();
+    for (; !isdigit(ch); ch = gc())
+      if (ch == '-')
+        neg = 1;
+    for (; isdigit(ch); ch = gc())
+      x = x * 10 + ch - '0';
+    if (ch == '.')
+      for (ch = gc(); isdigit(ch); ch = gc())
+        tmp /= 10.0, x += tmp *(ch - '0');
+    if (neg) 
+      x = -x;
+    return *this;
+  }
+  Input& operator>>(string& s) {
+    s.clear();
+    char ch = gc();
+    while (blank(ch)) ch = gc();
+    for (; !blank(ch); ch = gc())
+      s += ch;
+    return *this;
+  }
 } input;
 # define cin input
 
 struct Output {
-    struct setprecision {
-        int precision;
-    };
-    char pbuf[MAXSIZE], *pp = pbuf;
-    void push(const char &c) { 
-        if(pp - pbuf == MAXSIZE)                         
-            fwrite(pbuf, 1, MAXSIZE, stdout), pp = pbuf; 
-        *pp++ = c;\
-    }
-    int precision;
-    Output() { precision = 6;}
-    ~Output() { fwrite(pbuf, 1, pp - pbuf, stdout);}
-    char stk[40];
-    int top = 0;
-    template<class T>
-    Output &operator<<(const T &x) {
-        T tmp = x;
-        bool neg = tmp < 0;
-        if (neg) {
-            tmp *= -1;
-        }
-        for (; tmp; tmp /= 10) {
-            stk[++ top] = '0' + tmp % 10;
-        }
-        if (neg) {
-            stk[++ top] = '-';
-        }
-        for (; top; top --) {
-            push(stk[top]);
-        }
-        if (x == 0) {
-            push('0');
-        }
-        return *this;
-    }
-    Output &operator<<(const string &x) {
-        for (auto &u : x) {
-            push(u);
-        }
-        return *this;
-    }
-    template<size_t N>
-    Output &operator<<(const char(&x)[N]) {
-        *this << string(x);
-        return *this;
-    }
-    Output &operator<<(const char* const &x) {
-        for (const char* ptr = x; *ptr != '\0'; ++ptr) {
-            push(*ptr);
-        }
-        return *this;
-    }
-    Output &operator<<(const char &x) {
-        push(x);
-        return *this;
-    }
-    Output &operator<<(const bool &x) {
-        push(x ? '1' : '0');
-        return *this;
-    }
-    Output &operator<<(const double &x) {
-        int intPart = static_cast<int>(x);
-        *this << intPart;
+  char pbuf[kBufSize];
+  char *pp = pbuf;
+  char stk[40];
+  int top = 0;
 
-        push('.');
-        
-        double decimalPart = x - intPart;
-        for (int i = 0; i < precision; ++i) {
-            decimalPart *= 10;
-            int digit = static_cast<int>(decimalPart);
-            *this << char('0' + digit);
-            decimalPart -= digit;
-        }
-        return *this;
+  __attribute__((always_inline)) void push(char c) {
+    if (__builtin_expect(pp - pbuf == kBufSize, 0)) {
+      fwrite(pbuf, 1, kBufSize, stdout);
+      pp = pbuf;
     }
-    Output &operator<<(setprecision x) {
-        precision = x.precision;
-        return *this;
+    *pp++ = c;
+  }
+
+  ~Output() { fwrite(pbuf, 1, pp - pbuf, stdout); }
+
+  template<class T>
+  Output& operator<<(T x) {
+    if (x < 0) {
+      push('-');
+      x = -x;
     }
-    # undef push
-}output;
+    do {
+      stk[++top] = '0' + x % 10;
+      x /= 10;
+    } while (x);
+    while (top) {
+      push(stk[top--]);
+    }
+    return *this;
+  }
+
+  template<typename T>
+  typename std::enable_if<std::is_same<T, double>::value, Output&>::type
+  operator<<(const T&) = delete; // 不能输出double，因为4舍5入问题
+
+  Output& operator<<(const string& x) {
+    for (char u : x) {
+      push(u);
+    }
+    return *this;
+  }
+  template<size_t N>
+  Output& operator<<(const char(&x)[N]) {
+    *this << string(x);
+    return *this;
+  }
+  Output& operator<<(const char* const& x) {
+    for (const char* ptr = x; *ptr != '\0'; ++ptr) {
+      push(*ptr);
+    }
+    return *this;
+  }
+  Output& operator<<(char x) {
+    push(x);
+    return *this;
+  }
+  Output& operator<<(bool x) {
+    push(x ? '1' : '0');
+    return *this;
+  }
+} output;
 # define cout output
