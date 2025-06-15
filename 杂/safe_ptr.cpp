d67237ce1d559b5533bc746a4085ab124ccd4c54
ZKW465 <2695415649@qq.com>
2025-06-15 12:37:29 +0800

    safe_ptr update

--- 杂/safe_ptr.cpp
+++ 杂/safe_ptr.cpp
@@ -1,44 +1,24 @@
-/**
- * 1 MB = 1024 KB
- * 1 KB = 1024 B
- * 134'210'000    128
- * 262'144'000    256
- * 520'000'000    524
- * 1'030'000'000  1024
- * 注意事项：记得内存别开小了或者别爆了
- */
-
-constexpr int max_size = 1030000000;
-uint8_t buf[max_size];
-uint8_t* head = buf;
-
-using u32 = uint32_t;
+template<typename T>
+vector<T> pool;
 
 template<typename T>
-u32 alloc() {
-    return (head += sizeof(T)) - buf;
-}
+class safe_ptr {
+  int off;
+public:
+  safe_ptr(int off_ = 0) : off(off_) {}
 
-template<typename T>
-void dealloc() {
-    fill(buf, head + sizeof(T), 0);
-    head = buf;
-}   
-
-template <class T>
-struct safe_ptr {
-    u32 x;
-    safe_ptr(u32 x = 0) : x(x) {}
-    T* operator->() {
-        return (T*)(buf + x);
-    }
-    T& operator*() {
-        return *((T*)(buf + x));
-    }
-    operator u32() {
-        return x;
-    }
-    bool operator==(safe_ptr v) const {
-        return x == v.x;
-    }
+  T* operator->() {
+    return pool<T>.data() + off;
+  }
+  T& operator*() {
+    return pool<T>[off];
+  }
+  operator int() const { return off; }
+  bool operator==(safe_ptr v) { return off == v.off; }
 };
+
+template<typename T, typename... Args>
+safe_ptr<T> make_safe(Args&&... args) {
+  pool<T>.emplace_back(forward<Args>(args)...);
+  return safe_ptr<T>(pool<T>.size() - 1);
+}
