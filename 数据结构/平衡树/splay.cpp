5d3ffc97b5144c25cc368b517387c4e28e42893f
ZKW465 <2695415649@qq.com>
2025-06-17 15:52:32 +0800

    Update splay.cpp

--- 数据结构/平衡树/splay.cpp
+++ 数据结构/平衡树/splay.cpp
@@ -1,166 +1,169 @@
 struct node;
-using Tp = Base<node>;
+using Tp = safe_ptr<node>;
 struct node {
-    Tp ch[2], p;
-    i64 val;
-    i64 tag;
-    i64 sum;
-    int siz;
+  Tp ch[2], p;
+  int siz;
+  i64 val;
+  i64 sum;
+  i64 tag;
+  node(i64 val_) {
+    siz = 1;
+    val = val_;
+    sum = val;
+    tag = 0;
+  }
 };
 
-Tp alloc() {
-    return Tp::alloc();
+// must modify
+Tp make_sentinnel() {
+  Tp t = make_safe<node>(0);
+  t->siz = 0;
 }
 
-Tp alloc(auto val) {
-    Tp t = alloc();
-    t->val = val;
-    t->siz = 1;
-    t->sum = val;
-    t->tag = 0;
-    return t;
-}
+Tp sentinel = make_sentinnel();
 
-bool pos(Tp t) {
-    return t->p->ch[1] == t;
-}
+bool pos(Tp t) { return t->p->ch[1] == t; }
 
 void apply(Tp t, auto tag) {
-    if (t) {
-        t->val += tag;
-        t->sum += 1ll * t->siz * tag;
-        t->tag += tag;
-    }
+  if (t) {
+    t->val += tag;
+    t->sum += 1ll * t->siz * tag;
+    t->tag += tag;
+  }
 };
 
 void push(Tp t) {
-    if (t->tag) {
-        apply(t->ch[0], t->tag);
-        apply(t->ch[1], t->tag);
-        t->tag = decltype(t->tag)();
-    }
+  if (t->tag) {
+    apply(t->ch[0], t->tag);
+    apply(t->ch[1], t->tag);
+    t->tag = decltype(t->tag)();
+  }
 }
 
 void pull(Tp t) {
-    t->siz = t->ch[0]->siz + 1 + t->ch[1]->siz;
-    t->sum = t->ch[0]->sum + 1 + t->ch[1]->sum;
+  t->siz = t->ch[0]->siz + 1 + t->ch[1]->siz;
+  t->sum = t->ch[0]->sum + 1 + t->ch[1]->sum;
 }
 
 void rotate(Tp t) {
-    Tp q = t->p;
-    int x = !pos(t);
-    q->ch[!x] = t->ch[x];
-    if (t->ch[x]) t->ch[x]->p = q;
-    t->p = q->p;
-    if (q->p) q->p->ch[pos(q)] = t;
-    t->ch[x] = q;
-    q->p = t;
-    pull(q);
+  Tp q = t->p;
+  int x = !pos(t);
+  q->ch[!x] = t->ch[x];
+  if (t->ch[x])
+    t->ch[x]->p = q;
+  t->p = q->p;
+  if (q->p)
+    q->p->ch[pos(q)] = t;
+  t->ch[x] = q;
+  q->p = t;
+  pull(q);
 }
 
 void pushall(Tp t) {
-    if (t->p) pushall(t->p);
-    push(t);
+  if (t->p)
+    pushall(t->p);
+  push(t);
 }
 
 void splay(Tp t, Tp top = 0) {
-    pushall(t);
-    while (t->p != top) {               
-        if (t->p->p != top)
-            rotate(pos(t) ^ pos(t->p) ? t : t->p);
-        rotate(t);
-    }
-    pull(t);
+  pushall(t);
+  while (t->p != top) {
+    if (t->p->p != top)
+      rotate(pos(t) ^ pos(t->p) ? t : t->p);
+    rotate(t);
+  }
+  pull(t);
 }
 
 // to [-inf, val) and [val, inf]
 pair<Tp, Tp> split1(Tp t, auto x) {
-    if (!t) {
-        return {t, t};
+  if (!t) {
+    return {t, t};
+  }
+  Tp v = 0;
+  Tp j = t;
+  for (Tp i = t; i;) {
+    push(i);
+    j = i;
+    if (i->val >= x) {
+      v = i;
+      i = i->ch[0];
+    } else {
+      i = i->ch[1];
     }
-    Tp v = 0;
-    Tp j = t;
-    for (Tp i = t; i; ) {
-        push(i);
-        j = i;
-        if (i->val >= x) {
-            v = i;
-            i = i->ch[0];
-        } else {
-            i = i->ch[1];
-        }
-    }
-    
-    splay(j);
-    if (!v) {
-        return {j, 0};
-    }
-    
-    splay(v);
-    
-    Tp u = v->ch[0];
-    if (u) {
-        v->ch[0] = u->p = 0;
-        pull(v);
-    }
-    return {u, v};
+  }
+
+  splay(j);
+  if (!v) {
+    return {j, 0};
+  }
+
+  splay(v);
+
+  Tp u = v->ch[0];
+  if (u) {
+    v->ch[0] = u->p = 0;
+    pull(v);
+  }
+  return {u, v};
 }
 
 // 从 1 开始
-void splay(Tp& t, int k) {
-    while (true) {
-        push(t);
-        if (k > t->ch[0]->siz + 1) {
-            k -= t->ch[0]->siz + 1;
-            t = t->ch[1];
-        } else if (k <= t->ch[0]->siz) {
-            t = t->ch[0];
-        } else {
-            break;
-        }
+void splay(Tp &t, int k) {
+  while (true) {
+    push(t);
+    if (k > t->ch[0]->siz + 1) {
+      k -= t->ch[0]->siz + 1;
+      t = t->ch[1];
+    } else if (k <= t->ch[0]->siz) {
+      t = t->ch[0];
+    } else {
+      break;
     }
-    splay(t);
+  }
+  splay(t);
 }
 
 // to [1, x) and [x, n]
 pair<Tp, Tp> split2(Tp t, int x) {
-    if (t->siz < x) {
-        return {t, 0};
-    }
+  if (t->siz < x) {
+    return {t, 0};
+  }
 
-    splay(t, x);
-    
-    Tp u = t->ch[0];
-    if (u) {
-        t->ch[0] = u->p = 0;
-        pull(t);
-    }
-    return {u, t};
+  splay(t, x);
+
+  Tp u = t->ch[0];
+  if (u) {
+    t->ch[0] = u->p = 0;
+    pull(t);
+  }
+  return {u, t};
 }
 
 Tp merge(Tp l, Tp r) {
-    if (!l || !r) {
-        return l.x | r.x;
-    }
-    Tp i = l;
-    push(i);
-    while (i->ch[1]) {
-        i = i->ch[1], push(i);
-    }
-    splay(i);
-    i->ch[1] = r;
-    r->p = i;
-    pull(i);
-    return i;
+  if (!l || !r) {
+    return l | r;
+  }
+  Tp i = l;
+  push(i);
+  while (i->ch[1]) {
+    i = i->ch[1], push(i);
+  }
+  splay(i);
+  i->ch[1] = r;
+  r->p = i;
+  pull(i);
+  return i;
 }
 
 void dfs(Tp t, int dep = 0) {
-    if (!t) {
-        return;
-    }
-    push(t);
-    dfs(t->ch[0], dep + 1);
-    for (int i = 0; i < dep; i += 1) cerr << '\t';
-    cerr << t->val << "\n";
-    dfs(t->ch[1], dep + 1);
+  if (!t) {
+    return;
+  }
+  push(t);
+  dfs(t->ch[0], dep + 1);
+  for (int i = 0; i < dep; i += 1)
+    cerr << '\t';
+  cerr << t->val << "\n";
+  dfs(t->ch[1], dep + 1);
 }
