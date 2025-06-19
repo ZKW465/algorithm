2fc39c0d1c4f1ff9cd68a04c1f62593f8cd0cd70
ZKW465 <2695415649@qq.com>
2025-06-19 09:48:47 +0800

    Update 可变大小矩阵快速幂.cpp

--- 数学/可变大小矩阵快速幂.cpp
+++ 数学/可变大小矩阵快速幂.cpp
@@ -33,10 +33,11 @@ struct mat {
   }
   friend mat operator*(const mat &u, const mat &v) {
     mat z;
-    for (int k = 0; k < n; k += 1) {
-      for (int i = 0; i < n; i += 1) {
+    for (int i = 0; i < n; i += 1) {
+      for (int k = 0; k < n; k += 1) {
+        Z mul = u.a[i][k];
         for (int j = 0; j < n; j += 1) {
-          z.a[i][j] += u.a[i][k] * v.a[k][j];
+          z.a[i][j] += mul * v.a[k][j];
         }
       }
     }
