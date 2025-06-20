9098d5b499e18bf71d51945e8f83fc8f7f8c6f49
ZKW465 <2695415649@qq.com>
2025-06-20 17:15:06 +0800

    Update 带权树上倍增.cpp

--- 图论/带权树上倍增.cpp
+++ 图论/带权树上倍增.cpp
@@ -1,100 +1,97 @@
-template <class E> 
-struct csr {
-    vector<int> s;
-    vector<E> adj;
-    csr() {}
-    csr(int n, const vector<pair<int, E>>& g)
-        : s(n + 1), adj(g.size()) {
-        for (auto e : g) {
-            s[e.first + 1]++;
-        }
-        for (int i = 1; i <= n; i++) {
-            s[i] += s[i - 1];
-        }
-        auto c = s;
-        for (auto e : g) {
-            adj[c[e.first]++] = e.second;
-        }
+template <typename T> 
+class forest {
+public:
+  struct edge {
+    int from;
+    int to;
+    T cost;
+  };
+  int n;
+  vector<edge> edges;
+  vector<int> ids;
+  forest(int n_) : n(n_), ids(n_ + 1) {}
+  void add(int from, int to, T cost = 1) {
+    ids[from + 1]++;
+    ids[to + 1]++;
+    edges.emplace_back(from, to, cost);
+  }
+  void init() { 
+    for (int i = 1; i <= n; i++) {
+      ids[i] += ids[i - 1];
     }
+    auto cnt = ids;
+    auto last = edges;
+    for (auto& edge : last) {
+      edges[cnt[edge.first]++] = edge;
+    }
+  }
 };
 
-// static weighted tree
-template<int N = int(1E6)>
-struct multiTree {
-    int n;
-    int K;
-    vector<array<int, __lg(N) + 1>> p;
-    struct Edge {
-        int v; i64 w;
-        friend ostream &operator<<(ostream &cout, Edge u) {
-            return cout << "(" << u.v << ", " << u.w << ")";
-        } 
-    };
-    vector<pair<int, Edge>> e;
-    csr<Edge> g;
-    vector<int> dep;
-    vector<i64> dep2;
-    multiTree() {}
-    multiTree(int n) {
-        init(n);
+template<typename T, int N>
+class multiTree : public forest<T> {
+public:
+  using forest<T>::n;
+  using forest<T>::edges;
+  using forest<T>::ids;
+  int K;
+  vector<array<int, __lg(N) + 1>> p;
+  vector<int> dep;
+  vector<T> dep2;
+  multiTree();
+  multiTree(int n_) : forest<T>(n_), p(n_), dep(n), dep2(n), K(__lg(n) + 1) {}
+
+  void work(int root = 0) {
+    forest<T>::init();
+    dfs(root, root);
+  }
+
+  void dfs(int u, int f) {
+    p[u][0] = f;
+    for (int i = 1; i < K; i += 1) {
+      p[u][i] = p[p[u][i - 1]][i - 1];
     }
-    void init(int n) {
-        this->n = n;
-        K = __lg(n) + 1;
-        p.assign(n, {});
-        dep.assign(n, 0);
-        dep2.assign(n, 0);
+    for (int i = ids[u]; i < ids[u + 1]; i += 1) {
+      auto [from, to, cost] = edges[i];
+      int v = u ^ from ^ to;
+      if (v == f) {
+        continue;
+      }
+      dep[v] = dep[u] + 1;
+      dep2[v] = dep2[u] + cost;
+      dfs(v, u);
     }
-    void addEdge(int u, int v, auto w) {
-        e.push_back({u, {v, w}});
-        e.push_back({v, {u, w}});
+  }
+
+  int jump_to(int u, int d) {
+    for (int i = dep[u] - d; i > 0; i -= 1 << __lg(i)) {
+      u = p[u][__lg(i)];
     }
-    void work(int r = 0) {
-        g = csr<Edge>(n, e);
-        dfs(r, r);
+    return u;
+  }
+
+  int lca(int u, int v) {
+    if (dep[u] < dep[v]) {
+      swap(u, v);
     }
-    void dfs(int u, int f) {
-        p[u][0] = f;
-        for (int i = 1; i < K; i += 1) {
-            p[u][i] = p[p[u][i - 1]][i - 1];
-        }
-        for (int i = g.s[u]; i < g.s[u + 1]; i += 1) {
-            auto [v, w] = g.adj[i];
-            if (v == f) {
-                continue;
-            }
-            dep[v] = dep[u] + 1;
-            dep2[v] = dep2[u] + w;
-            dfs(v, u);
-        }
+    u = jump_to(u, dep[v]);
+    if (u == v) {
+      return u;
     }
-    int jump(int u, int d) {
-        for (int i = dep[u] - d; i > 0; i -= 1 << __lg(i)) {
-            u = p[u][__lg(i)];
-        }
-        return u;
+    for (int i = __lg(dep[u]); i >= 0; i -= 1) {
+      if (p[u][i] != p[v][i]) {
+        u = p[u][i], v = p[v][i];
+      }
     }
-    int lca(int u, int v) {
-        if (dep[u] < dep[v]) {
-            swap(u, v);
-        }
-        u = jump(u, dep[v]);
-        if (u == v) {
-            return u;
-        }
-        for (int i = __lg(dep[u]); i >= 0; i -= 1) {
-            if (p[u][i] != p[v][i]) {
-                u = p[u][i], v = p[v][i];
-            }
-        }
-        return p[u][0];
-    }
-    int dist(int u, int v) {
-        int m = lca(u, v);
-        return dep[u] + dep[v] - 2 * dep[m];
-    }
-    int dis2(int u, int v) {
-        int m = lca(u, v);
-        return dep2[u] + dep2[v] - 2 * dep[m];
-    }
+    return p[u][0];
+  }
+
+  int dist(int u, int v) {
+    int m = lca(u, v);
+    return dep[u] + dep[v] - 2 * dep[m];
+  }
+
+  T dis2(int u, int v) {
+    int m = lca(u, v);
+    return dep2[u] + dep2[v] - 2 * dep[m];
+  }
 };
